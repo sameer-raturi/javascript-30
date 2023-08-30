d89d2ce8396475e31490f48e255898a0b65caeea
Sameer_Raturi <sameerraturi2001@gmail.com>
2023-08-30 17:59:13 +0530

    flex panel

---
+++ 05.flex-panels-image-gallery/flex-panel.js
@@ -0,0 +1,8 @@
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen(){
+  this.classList.toggle('open');
+  this.classList.toggle('open-active');
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
